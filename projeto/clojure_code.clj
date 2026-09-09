b0ac7baabe0bd1cdf52a1de6a856be7146dd10c1
Geovanna Negreiros de Araújo <negreiros.geovanna@escolar.ifrn.edu.br>
2026-09-09 13:09:00 +0000

    adiciona o resto dos codigos

--- projeto/clojure_code.clj
+++ projeto/clojure_code.clj
@@ -1,3 +1,67 @@
+1-
+
+(defn executar-thread [id]
+      (Thread.
+        (fn []
+            (println "Thread" id "iniciada!")
+            (doseq [numero (range 1 6)]
+                   (println "Thread" id "- Número:" numero)
+                   (Thread/sleep 500))
+            (println "Thread" id "finalizada!"))))
+
+(defn main []
+      (let [thread1 (executar-thread 1)
+            thread2 (executar-thread 2)
+            thread3 (executar-thread 3)]
+
+           (.start thread1)
+           (.start thread2)
+           (.start thread3)
+
+           (.join thread1)
+           (.join thread2)
+           (.join thread3)
+
+           (println "Programa finalizado!")))
+
+(main)
+
+2- 
+
+(defn tempo-aleatorio []
+      (+ 200 (rand-int 801)))
+
+(defn criar-corredor [id]
+      (Thread.
+        (fn []
+            (println "Corredor" id "começou a corrida!")
+
+            (doseq [metro (range 1 11)]
+                   (Thread/sleep (tempo-aleatorio))
+                   (println "Corredor" id "chegou ao metro" metro))
+
+            (println "Corredor" id "terminou a corrida!"))))
+
+(defn main []
+      (let [corredor1 (criar-corredor 1)
+            corredor2 (criar-corredor 2)
+            corredor3 (criar-corredor 3)
+            corredor4 (criar-corredor 4)]
+
+           (.start corredor1)
+           (.start corredor2)
+           (.start corredor3)
+           (.start corredor4)
+
+           (.join corredor1)
+           (.join corredor2)
+           (.join corredor3)
+           (.join corredor4)
+
+           (println "Corrida finalizada!")))
+
+(main)
+
 (ns atv3)
 
 (defn worker
